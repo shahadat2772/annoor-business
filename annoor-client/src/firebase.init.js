1afb577261c1126abf238d18b10413e0c73f86c3
shahadat2772 <shahadat2772@gmailcom>
2022-11-28 07:52:42 +0600

    prepered for deploy

--- annoor-client/src/firebase.init.js
+++ annoor-client/src/firebase.init.js
@@ -6,12 +6,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId,
+
+  apiKey: "AIzaSyBFaD8TZGVax7OO37uaasODyH0mlnjd4J0",
+  authDomain: "annoor-business.firebaseapp.com",
+  projectId: "annoor-business",
+  storageBucket: "annoor-business.appspot.com",
+  messagingSenderId: "358329294791",
+  appId: "1:358329294791:web:ae088dc5110a475c5283e2",
 };
 
 // Initialize Firebase
--- annoor-server/index.js
+++ annoor-server/index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: __dirname + "/.env" });
+}
+
 const express = require("express");
 const app = express();
 const {
